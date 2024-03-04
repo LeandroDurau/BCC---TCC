1f0809bb88ce5176699ca56319005df737b9a2c9
jasminisantos <jasminirebeca10@gmail.com>
2024-03-04 18:24:02 -0300

    adding hills plotting to the graph

--- Projeto/main.py
+++ Projeto/main.py
@@ -6,32 +6,34 @@ import numpy as np
 df = pd.read_csv("./dados/Amaro_testeSL.csv", skiprows=[0,1,2,4,5], header=[0,1], nrows=7200)
 df = df.rename(columns={'Unnamed: 0_level_0': ''})
 
-pp = [] ## previousPoints
+pp = [] # previousPoints
 uh = False # Uphill
 mp = np.mean(df["Skeleton 018:RIPS","Z"]) #midpoint
-cs = [[],[]] #cicle start
-
+cs = [[],[]] #cycle start (for valleys)
+ps = [[],[]] #peak start (for hills)
 
 for index in range(len(df["Skeleton 018:RIPS","Z"])):
     value = df["Skeleton 018:RIPS","Z"][index]
     pp.append(value)
 
-    if len(pp) > 15 :
+    if len(pp) > 15:
         pp.pop(0)
     elif len(pp) < 15:
         continue
     
     mean = np.mean(pp)
-    if (mean < value and not uh and value < mp):
+    if mean < value and not uh and value < mp: 
         uh = True
         cs[0].append(index)
         cs[1].append(value)
-
-    if (mean > value and uh and value < mp):
+    elif mean > value and uh and value > mp:
         uh = False
+        ps[0].append(index)
+        ps[1].append(value)
 
-fig,ax = plt.subplots()
-sns.lineplot(y=df["Skeleton 018:RIPS","Z"], x = df["","Frame"])
+fig, ax = plt.subplots()
+sns.lineplot(y=df["Skeleton 018:RIPS","Z"], x=df["","Frame"])
 ax.scatter(cs[0], cs[1], c='firebrick', zorder=100)
+ax.scatter(ps[0], ps[1], c='royalblue', zorder=100)
 
 plt.show()
