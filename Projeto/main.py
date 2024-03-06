341ac86202de01a0def764eb06aa93ecfaf1755d
Leandro Ceron Durau <le_durau@hotmail.com>
2024-03-06 08:03:23 -0300

    Adiciona Analise
    + Adiciona primeira analise

--- Projeto/main.py
+++ Projeto/main.py
@@ -3,17 +3,28 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
+#katia - 12
+#nelson 16
+#Amaro - 18
+#Adilson - 19
+#Jair - 22
+#Marilene - 25
+#Izabel 27
+#Josui - 30
+#Carlos - 32
+#Arnaldo - 34
+
 #df = pd.read_csv("./dados/Amaro_testeSL.csv", skiprows=[0,1,2,4,5], header=[0,1], nrows=7200)
-df = pd.read_csv("./dados/Marilene_testeSL.csv", skiprows=[0,1,2,4,5], header=[0,1], nrows=7200)
+df = pd.read_csv("./dados/nelson_testeSL.csv", skiprows=[0,1,2,4,5], header=[0,1], nrows=7200)
 df = df.rename(columns={'Unnamed: 0_level_0': ''})
 
 columns = [
-        'Skeleton 025:LFTC', #0
-        'Skeleton 025:RIPS', #1
-        'Skeleton 025:Head', #2
-        'Skeleton 025:LShoulder', #3
-        'Skeleton 025:LUArm', #4
-        'Skeleton 025:LFLE', #5
+        'Skeleton 016:LFTC', #0
+        'Skeleton 016:RIPS', #1
+        'Skeleton 016:Head', #2
+        'Skeleton 016:LShoulder', #3
+        'Skeleton 016:LUArm', #4
+        'Skeleton 016:LFLE', #5
         ]
 
 selectedColumn = columns[0]
