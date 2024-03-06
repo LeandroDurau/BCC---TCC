4c809e5c0ca98692d1ed453b50a45db75af61289
Ercampa <enzocampa44@gmail.com>
2024-03-05 23:57:45 -0300

    Atualizações eurística

--- Projeto/main.py
+++ Projeto/main.py
@@ -6,32 +6,37 @@ import numpy as np
 df = pd.read_csv("./dados/Amaro_testeSL.csv", skiprows=[0,1,2,4,5], header=[0,1], nrows=7200)
 df = df.rename(columns={'Unnamed: 0_level_0': ''})
 
+selectedColumn = 'Skeleton 018:LFTC'
+
+max = df[selectedColumn, "Z"].max()
+min = df[selectedColumn, "Z"].min()
+globalMean = (max + min) / 2
+
 pp = [] ## previousPoints
 uh = False # Uphill
-mp = np.mean(df["Skeleton 018:RIPS","Z"]) #midpoint
 cs = [[],[]] #cicle start
 
 
-for index in range(len(df["Skeleton 018:RIPS","Z"])):
-    value = df["Skeleton 018:RIPS","Z"][index]
+for index in range(len(df[selectedColumn,"Z"])):
+    value = df[selectedColumn,"Z"][index]
     pp.append(value)
 
-    if len(pp) > 15 :
+    if len(pp) > 30 :
         pp.pop(0)
-    elif len(pp) < 15:
+    elif len(pp) < 30:
         continue
     
     mean = np.mean(pp)
-    if (mean < value and not uh and value < mp):
+    if (mean < value and not uh and value < globalMean):
         uh = True
         cs[0].append(index)
         cs[1].append(value)
 
-    if (mean > value and uh and value < mp):
+    if (mean > value and uh and value > globalMean):
         uh = False
 
 fig,ax = plt.subplots()
-sns.lineplot(y=df["Skeleton 018:RIPS","Z"], x = df["","Frame"])
+sns.lineplot(y=df[selectedColumn,"Z"], x = df["","Frame"])
 ax.scatter(cs[0], cs[1], c='firebrick', zorder=100)
 
 plt.show()
