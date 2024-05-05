3e50a1ad17b7feab958bf67ebb9a132d0ebe8213
Enzo Campa <enzocampa44@gmail.com>
2024-05-05 18:45:24 -0300

    correção main.py

--- Projeto/main.py
+++ Projeto/main.py
@@ -2,11 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-import scipy
 import sys
 
 id = sys.argv[1]
 axis = sys.argv[2]
+
 
 lista_pessoas = {
     '12': 'Katia',
@@ -23,7 +23,7 @@ lista_pessoas = {
 
 pessoa = lista_pessoas[id]
 
-df = pd.read_csv("./dados/"+pessoa+"_testeSL.csv", skiprows=[0,1,2,4,5], header=[0,1], nrows=7200)
+df = pd.read_csv("./dados/"+pessoa+"_testeSL.csv", skiprows=[0,1,2,4,5], header=[0,1], nrows=14400)
 df = df.rename(columns={'Unnamed: 0_level_0': ''})
 
 columns = [
@@ -54,37 +54,34 @@ cv = [[],[]] #cicle valley
 cp = [[],[]] #cicle peak
 
 
-cPeak = scipy.signal.find_peaks(df[selectedColumn, axis], prominence = 1)
-cPeak2 = map(lambda x: cp[0][x], cp[0])
+for index in range(len(df[selectedColumn,axis])):
+    value = df[selectedColumn,axis][index]
+    pp.append(value)
 
-# for index in range(len(df[selectedColumn,axis])):
-#     value = df[selectedColumn,axis][index]
-#     pp.append(value)
-#
-#     if len(pp) > 25 :
-#         pp.pop(0)
-#     elif len(pp) < 25:
-#         continue
-#   
-#     mean = np.mean(pp)
-#     if (mean < value and not uh and value < globalMean):
-#         uh = True
-#         cv[0].append(index)
-#         cv[1].append(df[selectedColumn2,axis][index])
-#
-#     if (mean > value and uh and value > globalMean):
-#         uh = False
-#         cp[0].append(index)
-#         cp[1].append(df[selectedColumn2,axis][index])
-#
-#
+    if len(pp) > 25 :
+        pp.pop(0)
+    elif len(pp) < 25:
+        continue
+   
+    mean = np.mean(pp)
+    if (mean < value and not uh and value < globalMean):
+        uh = True
+        cv[0].append(index)
+        cv[1].append(df[selectedColumn2,axis][index])
+
+    if (mean > value and uh and value > globalMean):
+        uh = False
+        cp[0].append(index)
+        cp[1].append(df[selectedColumn2,axis][index])
+
+
 fig,ax = plt.subplots()
-sns.lineplot(y=df[selectedColumn,axis], x = df["","Frame"])
-ax.scatter(cPeak, 0, c='firebrick', zorder=100)
-# ax.scatter(cp[0], cp[1], c='orange', zorder=100)
-#
-# for i in range(len(cv[0])):
-#     ax.annotate(str(i+1), (cp[0][i], cp[1][i] + 1))
-#     ax.annotate(str(i+1), (cv[0][i], cv[1][i] - 10))
-#
-# plt.show()
+sns.lineplot(y=df[selectedColumn2,axis], x = df["","Frame"])
+ax.scatter(cv[0], cv[1], c='firebrick', zorder=100)
+ax.scatter(cp[0], cp[1], c='orange', zorder=100)
+
+for i in range(len(cv[0])):
+    ax.annotate(str(i+1), (cp[0][i], cp[1][i] + 1))
+    ax.annotate(str(i+1), (cv[0][i], cv[1][i] - 10))
+
+plt.show()
