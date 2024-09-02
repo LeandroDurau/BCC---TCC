42bdfc3f2f2e1566c0e160a046e2df012a77a550
Enzo Campa <enzocampa44@gmail.com>
2024-09-01 22:24:56 -0300

    Correção classes

--- Projeto/main.py
+++ Projeto/main.py
@@ -8,10 +8,11 @@ import xlsxwriter
 import math
 from scipy.interpolate import UnivariateSpline
 
+
 id = sys.argv[1]
 axis = sys.argv[2]
 
-# Listagem de pessoas
+
 lista_pessoas = {
     '08': 'Teresinha',
     '09': 'Noemi',
@@ -37,15 +38,19 @@ lista_pessoas = {
 
 pessoa = lista_pessoas[id]
 
-# Criação do xlsx
 workbook = xlsxwriter.Workbook(f"normalizacao/normalizado-{pessoa}.xlsx")
 worksheet = workbook.add_worksheet()
 
-# Leitura de dados
+# workbook = xlsxwriter.Workbook(f"./anotacoes_realizadas/reestrutura_{pessoa}_{axis}.xlsx", {'nan_inf_to_errors': True})
+# worksheet = workbook.add_worksheet(axis)
+
 df = pd.read_csv(f"./dados/{pessoa}_testeSL.csv", skiprows=[0,1,2,4,5], header=[0,1])
 df = df.rename(columns={'Unnamed: 0_level_0': ''})
 
-# Listagem de pontos uteis
+dfs = pd.read_excel(f"./anotacoes_realizadas/{pessoa}.xlsx", sheet_name=f"{axis}")
+dfs = dfs.transpose()
+
+
 columns = [
     f'Skeleton 0{id}:LFTC', #0
     f"Skeleton 0{id}:RIPS", #1
@@ -55,29 +60,34 @@ columns = [
     f"Skeleton 0{id}:LFLE", #5
 ]
 
-# Seleção do ponto
 selectedColumn = columns[0]
 selectedColumn2 = columns[0]
 
-# Interpolação linear para remoção de NANs
+########################################################
+# print(df.head())
+# print('\n\n')
+# print(df.keys())
+# print('\n\n')
+
 df[selectedColumn,axis] = df[selectedColumn, axis].interpolate()
 
 max = df[selectedColumn, axis].max()
 min = df[selectedColumn, axis].min()
 globalMean = (max + min) / 2
 
-pp = [] # Previous points
+pp = [] ## previousPoints
 uh = False # Uphill
 
 cv = [[],[]] #cicle valley
 cp = [[],[]] #cicle peak
 
-pv = 0 # Peak ou valley (Para escrita das anotações no excel)
+helper = 1
+# helper2 = 1
+pv = 0
 
 pontos = []
 sizes = []
 
-# Variáveis para segmentação
 segmentos = []
 segmentos_temp = []
 last = 0
@@ -89,15 +99,12 @@ for index in range(len(df[selectedColumn,axis])):
     # Segmentando o dataset
     segmentos_temp.append(value)
 
-    # Manter pp com 25 pontos
     if len(pp) > 25 :
         pp.pop(0)
     elif len(pp) < 25:
         continue
    
     mean = np.mean(pp)
-
-    #Lógica para achar vales
     if (mean < value and not uh and value < globalMean):
         # Muda a direção
         uh = True
@@ -114,10 +121,9 @@ for index in range(len(df[selectedColumn,axis])):
         segmentos.append(segmentos_temp.copy())
         segmentos_temp = []
         pv = 2
-        # helper += 1
+        helper += 1
         
 
-    #Lógica para achar picos
     if (mean > value and uh and value > globalMean):
         # Muda a direção
         uh = False
@@ -145,12 +151,24 @@ col = 1
 up_idx=1
 down_idx=1
 
-# Interpolação dos segmentos para terem o mesmo tamanho
 for value in segmentos:
     old_indexes = np.arange(0, len(value))
     new_indexes = np.linspace(0, len(value) - 1, int(sizes_median))
     spl = UnivariateSpline(old_indexes, value, k=3, s=0)
     new_val = spl(new_indexes)
+
+    # Classes
+    if value[0] > value[-1]:
+        # O ultimo n foi classificado nesse caso
+        if (down_idx not in dfs[2]):
+            down_idx -= 1
+        worksheet.write(row,0, dfs[2][down_idx])
+        down_idx += 1
+    else:
+        if (up_idx not in dfs[2]):
+            up_idx -= 1
+        worksheet.write(row,0, dfs[0][up_idx])
+        up_idx += 1
 
     for item in new_val:
         worksheet.write(row, col, item)
@@ -160,3 +178,4 @@ for value in segmentos:
 
 workbook.close()
 exit()
+
