969255126f27f944c23198c0c446bc2a3e192a93
Leandro Ceron Durau <le_durau@hotmail.com>
2024-10-07 07:00:38 -0300

    Update TCC
    + Adiciona KNN com K-FOLDS
    + Adiciona RandomForest com K-FOLDS

--- Projeto/visualizador de dados.py
+++ Projeto/visualizador de dados.py
@@ -13,15 +13,19 @@ import plotly.offline
 
 # Incorporate data
 #df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
-df = pd.read_excel("resultados/ilda.xlsx")
+df = pd.read_excel("resultados/Teresinha_rf.xlsx")
 df = df.sort_values(by="y")
 
-df_1 = df[df['label'] == 1]
-df_0 = df[df['label'] == 0]
+df_tp = df[df['label'] == 'TP']
+df_tn = df[df['label'] == 'TN']
+df_fp = df[df['label'] == 'FP']
+df_fn = df[df['label'] == 'FN']
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=df_1['y'], y=df_1['x'], mode='lines', name=f'Eixo', line=dict(color="red")))
-fig.add_trace(go.Scatter(x=df_0['y'], y=df_0['x'], mode='lines', name=f'Eixo', line=dict(color="blue")))
+fig.add_trace(go.Scatter(x=df_tp['y'], y=df_tp['x'], mode='lines', name=f'TP', line=dict(color="green")))
+fig.add_trace(go.Scatter(x=df_tn['y'], y=df_tn['x'], mode='lines', name=f'TN', line=dict(color="royalblue")))
+fig.add_trace(go.Scatter(x=df_fp['y'], y=df_fp['x'], mode='lines', name=f'FP', line=dict(color="firebrick")))
+fig.add_trace(go.Scatter(x=df_fn['y'], y=df_fn['x'], mode='lines', name=f'FN', line=dict(color="yellow")))
 
-path = './graficos/ilda.html'
+path = './graficos/Teresinha_rf.html'
 plotly.offline.plot(fig, filename = path, auto_open=False)
